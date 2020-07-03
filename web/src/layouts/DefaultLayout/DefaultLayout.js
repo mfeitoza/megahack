e8e82603c5eb1e9fa2c7f3c4b2210dc866bfbbd6
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2020-07-03 01:35:30 -0300

    feat(layouts): add zeit page

--- web/src/layouts/DefaultLayout/DefaultLayout.js
+++ web/src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,5 +1,13 @@
+import { Page } from '@zeit-ui/react'
+
 const DefaultLayout = ({ children }) => {
-  return <>{children}</>
+  return (
+    <>
+      <Page>
+        <main>{children}</main>
+      </Page>
+    </>
+  )
 }
 
 export default DefaultLayout
