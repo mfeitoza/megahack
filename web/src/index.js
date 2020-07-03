d1d604454cf874932ff1bda0cf26119aaf397011
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2020-07-03 01:21:23 -0300

    feat: add zeit-ui

--- web/src/index.js
+++ web/src/index.js
@@ -1,16 +1,20 @@
 import ReactDOM from 'react-dom'
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
+import { ZeitProvider, CssBaseline } from '@zeit-ui/react'
 
 import Routes from 'src/Routes'
 
 import './index.css'
 
 ReactDOM.render(
-  <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider>
-      <Routes />
-    </RedwoodProvider>
-  </FatalErrorBoundary>,
+  <ZeitProvider>
+    <CssBaseline />
+    <FatalErrorBoundary page={FatalErrorPage}>
+      <RedwoodProvider>
+        <Routes />
+      </RedwoodProvider>
+    </FatalErrorBoundary>
+  </ZeitProvider>,
   document.getElementById('redwood-app')
 )
