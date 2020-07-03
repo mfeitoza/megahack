3c11f5614a483260f65516a97ff18cc6035e89d0
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2020-07-03 01:21:40 -0300

    feat(page): add login page

---
+++ web/src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,10 @@
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>Find me in ./web/src/pages/LoginPage/LoginPage.js</p>
+    </div>
+  )
+}
+
+export default LoginPage
