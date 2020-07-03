355889e2ab0203213fa291c406f7e14f140e8852
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2020-07-03 11:22:50 -0300

    style(Routes.js)

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -13,7 +13,11 @@ const Routes = () => {
   return (
     <Router>
       <Route path="/categories/new" page={NewCategoryPage} name="newCategory" />
-      <Route path="/categories/{id:Int}/edit" page={EditCategoryPage} name="editCategory" />
+      <Route
+        path="/categories/{id:Int}/edit"
+        page={EditCategoryPage}
+        name="editCategory"
+      />
       <Route path="/categories/{id:Int}" page={CategoryPage} name="category" />
       <Route path="/categories" page={CategoriesPage} name="categories" />
       <Route path="/home" page={HomePage} name="home" />
