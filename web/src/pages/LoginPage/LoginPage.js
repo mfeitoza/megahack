b9106e7fed8c3a673e5c74ce27bdf22fbe75c073
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2020-07-03 01:35:43 -0300

    feat(pages): add login page

--- web/src/pages/LoginPage/LoginPage.js
+++ web/src/pages/LoginPage/LoginPage.js
@@ -1,9 +1,26 @@
+import { Input, Spacer, Button } from '@zeit-ui/react'
+import DefaultLayout from 'src/layouts/DefaultLayout'
+
 const LoginPage = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <p>Find me in ./web/src/pages/LoginPage/LoginPage.js</p>
-    </div>
+    <DefaultLayout>
+      <div>
+        <h1>Login Page</h1>
+        <form>
+          <Input placeholder="diego@email.com.br" width="50%">
+            Email
+          </Input>
+          <Spacer y={0.5} />
+          <Input.Password placeholder="••••••••" width="50%">
+            Senha
+          </Input.Password>
+          <Spacer y={0.5} />
+          <Button type="success" auto>
+            Entrar
+          </Button>
+        </form>
+      </div>
+    </DefaultLayout>
   )
 }
 
