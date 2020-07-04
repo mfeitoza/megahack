15146e33f9cbe4b676233c71dcb00c16d9045b2d
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2020-07-04 18:53:31 -0300

    feat(api): add seed file

--- api/prisma/seeds.js
+++ api/prisma/seeds.js
@@ -5,6 +5,156 @@ const dotenv = require('dotenv')
 dotenv.config()
 const db = new PrismaClient()
 
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array)
+  }
+}
+
+const tags = [
+  { name: 'queijo' },
+  { name: 'bebida' },
+  { name: 'carne' },
+  { name: 'gráfica' },
+  { name: 'reparo' },
+]
+
+const users = [
+  {
+    email: 'ptejero0@google.cn',
+    name: 'Phaedra Tejero',
+    phone: '4975453821',
+  },
+  {
+    email: 'mcreaser1@t-online.de',
+    name: 'Morgen Creaser',
+    phone: '4308177426',
+  },
+  {
+    email: 'ghadny2@craigslist.org',
+    name: 'Guthry Hadny',
+    phone: '6428910280',
+  },
+  {
+    email: 'dorme3@youtu.be',
+    name: 'Dulcinea Orme',
+    phone: '3248615369',
+  },
+  {
+    email: 'clikly4@canalblog.com',
+    name: 'Collin Likly',
+    phone: '3191802366',
+  },
+  {
+    email: 'afronks5@dot.gov',
+    name: 'Agneta Fronks',
+    phone: '3928462120',
+  },
+  {
+    email: 'idominicacci6@slate.com',
+    name: 'Irene Dominicacci',
+    phone: '8176721188',
+  },
+  {
+    email: 'mhallihan7@gov.uk',
+    name: 'Mareah Hallihan',
+    phone: '2185546344',
+  },
+  {
+    email: 'mmclernon8@yelp.com',
+    name: 'Mannie McLernon',
+    phone: '1273128934',
+  },
+  {
+    email: 'awalley9@timesonline.co.uk',
+    name: 'Ashby Walley',
+    phone: '2433736697',
+  },
+]
+
+const companies = [
+  {
+    company: 'Rhybox',
+    isSupplier: true,
+    zipCode: '00214',
+    address: '34 Vahlen Park',
+    state: 'New Hampshire',
+    city: 'Portsmouth',
+  },
+  {
+    company: 'Youspan',
+    isSupplier: false,
+    zipCode: '65218',
+    address: '6837 6th Court',
+    state: 'Missouri',
+    city: 'Columbia',
+  },
+  {
+    company: 'Voomm',
+    isSupplier: true,
+    zipCode: '17622',
+    address: '26333 Shoshone Pass',
+    state: 'Pennsylvania',
+    city: 'Lancaster',
+  },
+  {
+    company: 'Thoughtworks',
+    isSupplier: true,
+    zipCode: '44305',
+    address: '9 Menomonie Circle',
+    state: 'Ohio',
+    city: 'Akron',
+  },
+  {
+    company: 'Browseblab',
+    isSupplier: false,
+    zipCode: '85305',
+    address: '144 Browning Way',
+    state: 'Arizona',
+    city: 'Glendale',
+  },
+  {
+    company: 'Wordify',
+    isSupplier: false,
+    zipCode: '37450',
+    address: '70 Graedel Crossing',
+    state: 'Tennessee',
+    city: 'Chattanooga',
+  },
+  {
+    company: 'Oyoloo',
+    isSupplier: true,
+    zipCode: '90040',
+    address: '20 Harbort Circle',
+    state: 'California',
+    city: 'Los Angeles',
+  },
+  {
+    company: 'Thoughtmix',
+    isSupplier: false,
+    zipCode: '23509',
+    address: '5 3rd Way',
+    state: 'Virginia',
+    city: 'Norfolk',
+  },
+  {
+    company: 'Meembee',
+    isSupplier: true,
+    zipCode: '48919',
+    address: '7 Independence Place',
+    state: 'Michigan',
+    city: 'Lansing',
+  },
+  {
+    company: 'Mynte',
+    isSupplier: false,
+    zipCode: '22405',
+    address: '63 Redwing Place',
+    state: 'Virginia',
+    city: 'Fredericksburg',
+  },
+]
+
 async function main() {
   // Seed data is database data that needs to exist for your app to run.
   // Ideally this file should be idempotent: running it multiple times
@@ -15,6 +165,28 @@ async function main() {
   //   if (!existing.length) {
   //     await db.user.create({ data: { name: 'Admin', email: 'admin@email.com' }})
   //   }
+  const createdUsers = []
+
+  await asyncForEach(users, async (user) => {
+    console.log(`Creating ${user.email}...`)
+    createdUsers.push(
+      await db.user.create({
+        data: user,
+      })
+    )
+  })
+
+  await asyncForEach(companies, async (company, index) => {
+    console.log(`Creating ${company.company}...`)
+    await db.company.create({
+      data: {
+        ...company,
+        user: {
+          connect: { id: createdUsers[index].id },
+        },
+      },
+    })
+  })
 
   console.info('No data to seed. See api/prisma/seeds.js for info.')
 }
