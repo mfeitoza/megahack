ce7c38721833452ab440aa4174be2128d5aa4503
Marcus Feitoza <marcus.feitoza.filho@gmail.com>
2020-07-04 20:08:03 -0300

    fix(web): remove button from home

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -8,14 +8,6 @@ const HomePage = () => {
   return (
     <AppLayout>
       <Grid.Container gap={2} justify="center">
-        <Grid>
-          <Link to={routes.newRequest()}>
-            <Button type="success" shadow>
-              Criar requisição
-            </Button>
-          </Link>
-        </Grid>
-
         <Grid xs={24}>
           <ActiveRequestsCell />
         </Grid>
